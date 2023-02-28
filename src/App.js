30d0055bf0af6750cfdfdfc08e256fde571c1a5b
nomliF <112778762+nomliF@users.noreply.github.com>
2023-02-28 16:22:29 +0100

    Added some

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Book from "./pages/Book"
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/book" exact component={Book} />
+          <Route path="login" exact component={Login}/>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/BookItem.js
@@ -0,0 +1,33 @@
+//import Logo from "../assets/logo2.svg";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+
+function BookItem() {
+    return (
+        <div className="bookItem">
+            <div className="leftSide">
+                {/*<img src={Logo} alt={Logo}/>
+*/}                <div className="hiddenLinks">
+                    <Link to="/booklist"> Preview </Link>
+                    <Link to="/about"> Summary </Link>
+                    <Link to="/book"> Review </Link>
+                    <Link to="/contact"> FavoriteD </Link>
+                    <Link to="/contact"> FavoriteQ </Link>
+                    <Link to="/contact"> FavoriteA </Link>
+
+                </div>
+            </div>
+            <div className="rightSide">
+                <Link to="/booklist"> Preview </Link>
+                <Link to="/book"> Review </Link>
+                <Link to="/about"> Summary </Link>
+                <Link to="/contact"> FavoriteD </Link>
+                <Link to="/contact"> FavoriteQ </Link>
+                <Link to="/contact"> FavoriteA </Link>
+            </div>
+        </div>
+    );
+}
+
+export default BookItem;
