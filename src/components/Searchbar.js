a6d59ac9e7293bc616bba4e7251220b4fffc35cd
reeshy <64738767+reeshy@users.noreply.github.com>
2023-03-05 22:29:42 +0100

    Fixed error not allowing us to access the navbar from contacts.js

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div className="search-area">
+      <form action="">
+        <input type="text" />
+      </form>
+    </div>
+  );
+};
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,55 +5,52 @@ import "../styles/Contact.css";
 function Contact() {
   return (
     <div className="contact">
-      <div
-        className="leftSide"
-        style={{ backgroundImage: `url(${logo1})` }}
-      ></div>
+      <div className="leftSide" style={{ backgroundImage: `url(${logo1})` }}>
+        <div></div>
+      </div>
       <div className="rightSide">
         <h1> Contact Us</h1>
 
-        <div className="container">
-          <form
-            target="_blank"
-            action="https://formsubmit.co/raah19ls@student.ju.se"
-            method="POST"
-          >
-            <div className="form-group">
-              <div className="form-row">
-                <div className="col">
-                  <input
-                    type="text"
-                    name="name"
-                    className="form-control rounded-pill"
-                    placeholder="Full Name"
-                    required
-                  />
-                </div>
-                <div className="col">
-                  <input
-                    type="email"
-                    name="email"
-                    className="form-control rounded-pill"
-                    placeholder="Email Address"
-                    required
-                  />
-                </div>
+        <form
+          target="_blank"
+          action="https://formsubmit.co/raah19ls@student.ju.se"
+          method="POST"
+        >
+          <div className="form-group">
+            <div className="form-row">
+              <div className="col">
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control rounded-pill"
+                  placeholder="Full Name"
+                  required
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control rounded-pill"
+                  placeholder="Email Address"
+                  required
+                />
               </div>
             </div>
-            <div className="form-group">
-              <textarea
-                placeholder="Your Message"
-                className="form-control rounded"
-                name="message"
-                rows="10"
-                required
-              ></textarea>
-            </div>
-            <button type="submit" className="btn btn-lg btn-dark btn-block">
-              Submit Form
-            </button>
-          </form>
-        </div>
+          </div>
+          <div className="form-group">
+            <textarea
+              placeholder="Your Message"
+              className="form-control rounded"
+              name="message"
+              rows="10"
+              required
+            ></textarea>
+          </div>
+          <button type="submit" className="btn btn-lg btn-dark btn-block">
+            Submit Form
+          </button>
+        </form>
       </div>
     </div>
   );
